4e47eceee62c10808f55fcc44175da0d56dbc724
dog-industry <91227995+dog-industry@users.noreply.github.com>
2022-06-30 11:45:17 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function take_snapshot()
 
   console.log('ml5 version:', ml5.version);
   
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/8aKpS4kd3/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/cDKf-BFFM/model.json',modelLoaded);
 
   function modelLoaded() {
     console.log('Model Loaded!');
